308eea7e22c1511bf87be0604b82066d7034f1c8
canghai118 <1471192800@qq.com>
2022-12-25 10:56:22 +0800

    save

--- .z-rc.d/60-node.sh
+++ .z-rc.d/60-node.sh
@@ -3,3 +3,11 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+if [ -d "$PNPM_HOME" ]; then
+  export PATH="$PNPM_HOME:$PATH"
+fi
+# pnpm end #
